39c6c6c723f559def45c23a6d810326aa87ec8b7
UplandJacob <123428589+UplandJacob@users.noreply.github.com>
2024-12-08 16:41:04 -0600

    Update java-dl.sh

--- java-dl.sh
+++ java-dl.sh
@@ -8,34 +8,37 @@ case $ARCH in
         URL="https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.5%2B11/OpenJDK21U-jdk_aarch64_linux_hotspot_21.0.5_11.tar.gz"
         ;;
     armv7)
-        URL="https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.5%2B11/OpenJDK21U-jdk_aarch64_linux_hotspot_21.0.5_11.tar.gz"
+        URL=""
+        apt-get install default-jre #java 17
         ;;
     armhf)
-        URL="https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.5%2B11/OpenJDK21U-jdk_aarch64_linux_hotspot_21.0.5_11.tar.gz"
+        URL=""
+        apt-get install default-jre #java 17
         ;;
     amd64)
         URL="https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.5%2B11/OpenJDK21U-jdk_x64_linux_hotspot_21.0.5_11.tar.gz"
         ;;
     i386)
-        URL="https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21%2B36/OpenJDK21U-jdk_i386_linux_hotspot_21.0.0_36.tar.gz"
+        apt-get install default-jre #java 17
         ;;
     *)
         echo "Unsupported architecture: $ARCH"
         exit 1
         ;;
 esac
-
-# Download and extract the package
-wget -q -O jdk-21.tar.gz $URL
-tar -xzf jdk-21.tar.gz -C /usr/local
-
-# Set JAVA_HOME and update PATH
-JAVA_HOME=$(find /usr/local -maxdepth 1 -type d -name "jdk-21*")
-echo "export JAVA_HOME=$JAVA_HOME" >> ~/.bashrc
-echo "export PATH=\$JAVA_HOME/bin:\$PATH" >> ~/.bashrc
-
-# Apply changes to PATH
-source ~/.bashrc
+if [[ ! -z "$URL" ]]; then
+    # Download and extract the package
+    wget -q -O jdk-21.tar.gz $URL
+    tar -xzf jdk-21.tar.gz -C /usr/local
+    
+    # Set JAVA_HOME and update PATH
+    JAVA_HOME=$(find /usr/local -maxdepth 1 -type d -name "jdk-21*")
+    echo "export JAVA_HOME=$JAVA_HOME" >> ~/.bashrc
+    echo "export PATH=\$JAVA_HOME/bin:\$PATH" >> ~/.bashrc
+    
+    # Apply changes to PATH
+    source ~/.bashrc
+fi
 
 echo "Java version:"
 java --version
