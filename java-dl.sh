1496836f199bb39c5b3dbd02284dd36fe6806838
UplandJacob <123428589+UplandJacob@users.noreply.github.com>
2024-12-08 13:37:49 -0600

    java-dl.sh

---
+++ java-dl.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ARCH=$(uname -m)
+
+case $ARCH in
+    aarch64)
+        URL="https://download.oracle.com/java/21/latest/jdk-21_linux-aarch64_bin.deb"
+        ;;
+    armv7)
+        URL="https://download.oracle.com/java/21/latest/jdk-21_linux-armv7_bin.deb"
+        ;;
+    armhf)
+        URL="https://download.oracle.com/java/21/latest/jdk-21_linux-armhf_bin.deb"
+        ;;
+    amd64)
+        URL="https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.deb"
+        ;;
+    i386)
+        URL="https://download.oracle.com/java/21/latest/jdk-21_linux-i386_bin.deb"
+        ;;
+    *)
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+        ;;
+esac
+
+wget -O jdk-21.deb $URL
+
+sudo dpkg -i jdk-21.deb
+
+echo "Java 21 installed successfully!"
