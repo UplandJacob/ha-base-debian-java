c77fc87f4e9f1374a70df3da6a8a6bcf9df6f170
UplandJacob <123428589+UplandJacob@users.noreply.github.com>
2024-12-08 15:03:25 -0600

    Update java-dl.sh

--- java-dl.sh
+++ java-dl.sh
@@ -10,14 +10,14 @@ case $ARCH in
         tar -xzf jdk-21.tar.gz
         ;;
     armv7)
-        URL="https://download.oracle.com/java/21/latest/jdk-21_linux-aarch64_bin.tar.gz"
-        wget -q -O jdk-21.tar.gz $URL
-        tar -xzf jdk-21.tar.gz
+        URL="wget https://openems.io/download/temurin-21-jdk-armv7_21.0.6+2.deb"
+        wget -q -O jdk-21.deb $URL
+        dpkg -i jdk-21.deb
         ;;
     armhf)
-        URL="https://download.oracle.com/java/21/latest/jdk-21_linux-aarch64_bin.tar.gz"
-        wget -q -O jdk-21.tar.gz $URL
-        tar -xzf jdk-21.tar.gz
+        URL="wget https://openems.io/download/temurin-21-jdk-armhf_21.0.6+2.deb"
+        wget -q -O jdk-21.deb $URL
+        dpkg -i jdk-21.deb
         ;;
     amd64)
         URL="https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.deb"
@@ -25,9 +25,7 @@ case $ARCH in
         dpkg -i jdk-21.deb
         ;;
     i386)
-        URL="wget https://openems.io/download/temurin-21-jdk-armhf_21.0.6+2.deb"
-        wget -q -O jdk-21.deb $URL
-        dpkg -i jdk-21.deb
+        
         ;;
     *)
         echo "Unsupported architecture: $ARCH"
