8eb97fee227c5a1cf8a0b548fdf130dd60c2dd19
UplandJacob <123428589+UplandJacob@users.noreply.github.com>
2025-05-20 17:29:40 -0500

    Update java-dl.sh

--- java-dl.sh
+++ java-dl.sh
@@ -5,18 +5,22 @@ echo "arch: $ARCH"
 
 case $ARCH in
     aarch64)
-        URL="https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.7%2B6/OpenJDK21U-jre_aarch64_linux_hotspot_21.0.7_6.tar.gz"
+        wget https://packages.microsoft.com/config/debian/$(lsb_release -rs)/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+        dpkg -i packages-microsoft-prod.deb
+        apt update
+        apt install msopenjdk-21
         ;;
     armv7)
-        URL=""
         apt-get install -y default-jre #java 17
         ;;
     armhf)
-        URL=""
         apt-get install -y default-jre #java 17
         ;;
     amd64)
-        URL="https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.7%2B6/OpenJDK21U-jre_x64_linux_hotspot_21.0.7_6.tar.gz"
+        wget https://packages.microsoft.com/config/debian/$(lsb_release -rs)/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+        dpkg -i packages-microsoft-prod.deb
+        apt update
+        apt install msopenjdk-21
         ;;
     i386)
         apt-get install -y default-jre #java 17
@@ -26,19 +30,7 @@ case $ARCH in
         exit 1
         ;;
 esac
-if [[ ! -z "$URL" ]]; then
-    # Download and extract the package
-    wget -q -O jdk-21.tar.gz $URL
-    tar -xzf jdk-21.tar.gz -C /usr/local
-    
-    # Set JAVA_HOME and update PATH
-    JAVA_HOME=$(find /usr/local -maxdepth 1 -type d -name "jdk-21*")
-    echo "export JAVA_HOME=$JAVA_HOME" >> ~/.bashrc
-    echo "export PATH=\$JAVA_HOME/bin:\$PATH" >> ~/.bashrc
-    
-    # Apply changes to PATH
-    source ~/.bashrc
-fi
+
 
 echo "Java version:"
 java --version
